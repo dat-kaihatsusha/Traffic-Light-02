b5349d7214859beece12f128e5fe5f285c164be0
DatNT117 <nguyendatmta@gmail.com>
2021-10-05 22:23:04 +0700

    Dieu khien tu app component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 import TrafficLight from './components/TrafficLight';
 import './App.css';
 
+const RED = 0;
+const ORANGE = 1;
+const GREEN = 2;
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentColor: GREEN
+    };
+
+    setInterval(() => {
+      this.setState({ currentColor: this.getNextColor(this.state.currentColor) });
+    }, 500);
+  }
+
+  getNextColor(color) {
+    switch (color) {
+      case RED: return ORANGE;
+      case ORANGE: return GREEN;
+      default: return RED;
+    }
+  }
+
   render() {
+    const {currentColor} = this.state;
     return (
       <div className="App">
         nguyen tien dat
-        <TrafficLight />
+        <TrafficLight currentColor={currentColor}/>
       </div>
     );
   }
--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -7,27 +7,10 @@ const ORANGE = 1;
 const GREEN = 2;
 
 class TrafficLight extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentColor: GREEN
-    };
-
-
-    setInterval(() => {
-      this.setState({ currentColor: this.getNextColor(this.state.currentColor) });
-    }, 1000);
-  }
-
-  getNextColor(color) {
-    switch (color) {
-      case RED: return ORANGE;
-      case ORANGE: return GREEN;
-      default: return RED;
-    }
-  }
+  
   render() {
-    const { currentColor } = this.state;
+    const { currentColor } = this.props;
+    console.log(this.props);
     return <div className="TrafficLight">
       <div className={classNames('bulb', 'red', {
         active: currentColor === RED
